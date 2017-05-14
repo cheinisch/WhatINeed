d2e582d19351af3fe4b043718bc42ecaabf8f498
cheinisch <chris@christian-heinisch.de>
2017-05-14 13:43:18 +0200

    Menü definiert

--- app/src/main/java/de/christian_heinisch/whatineed/MainActivity.java
+++ app/src/main/java/de/christian_heinisch/whatineed/MainActivity.java
@@ -1,9 +1,7 @@
 package de.christian_heinisch.whatineed;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.v4.app.FragmentManager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -22,15 +20,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -83,7 +72,7 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_information) {
             // Handle the camera action
         } else if (id == R.id.nav_errorcode) {
-
+            errorcode();
         } else if (id == R.id.nav_about) {
 
         }
@@ -91,5 +80,26 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void titelleiste(String title){
+
+        setTitle(title);
+
+    }
+
+    public void errorcode() {
+
+        titelleiste("Fehlercodes");
+
+        ErrorCode_Fragment overviewFragment = new ErrorCode_Fragment();
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(
+                R.id.content_main,
+                overviewFragment,
+                overviewFragment.getTag()
+        )
+                .addToBackStack(null)
+                .commit();
     }
 }
