b2c95a278992d3d64a889d9aa2bceaac41cce459
cheinisch <chris@christian-heinisch.de>
2017-07-28 21:13:28 +0200

    Add MessageActivity

---
+++ app/src/main/java/de/christian_heinisch/hilferundumskfz/MessageActivity.java
@@ -0,0 +1,13 @@
+package de.christian_heinisch.hilferundumskfz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+    }
+}
